4bbe5ab8b6ab1a293a38547e96520018af2f2a0b
miavai649 <mahmudulhaquenoor@gmail.com>
2023-03-27 17:50:22 +0600

    first commit

---
+++ schema/type-defs.js
@@ -0,0 +1,28 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+    type User {
+        id: ID!
+        name: String!
+        username: String!
+        age: Int!
+        nationality: Nationality!
+    }
+
+    type Query{
+        users: [User!]!
+
+        
+    }
+
+    enum Nationality{
+            CANADA
+            BRAZIL
+            INDIA
+            GERMANY 
+            CHILE
+        }
+
+`;
+
+module.exports = { typeDefs };
